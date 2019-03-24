7ad116ec6f6772a88e56909ee524c405362ce5e0
Thomas Kelly <tkelly2@tcd.ie>
2019-03-24 12:15:54 +0000

    All functions working

--- StopwatchCode/stop_watch_if.v
+++ StopwatchCode/stop_watch_if.v
@@ -2,8 +2,8 @@
 module stop_watch_if
    (
     input wire clk,
-    input wire go, clr,
-    output wire [3:0] d3, d2, d1, d0 // 4 digits  
+    input wire go, clr,up, //added input 'up' switch. 
+    output wire [3:0] d3, d2, d1, d0 // added minute digit, 4 digits  
    );
 
    // declaration
@@ -27,10 +27,10 @@ module stop_watch_if
 
    // next-state logic
    // 0.1 sec tick generator: mod-5000000
-   assign ms_next = (clr || (ms_reg==DVSR && go)) ? 4'b0 : //if clr == high OR ms_reg == high AND go == high -> next = 1 
+   assign ms_next = (clr || (ms_reg==DVSR && go)) ? 4'b0 : // if(clr == high OR ms_reg == DVSR AND go == true) -then-> ms_next = 0; 
                     (go) ? ms_reg + 1 : //else if go == high -> ms_reg++ 
                            ms_reg; //else next = ms_reg 
-   assign ms_tick = (ms_reg==DVSR) ? 1'b1 : 1'b0;
+   assign ms_tick = (ms_reg==DVSR) ? 1'b1 : 1'b0; //if(ms_reg == DVSR) -> ms_tick = 1; otherwise ms_tick = 0; 
    // 3-digit bcd counter
    always @*
    begin
@@ -40,36 +40,71 @@ module stop_watch_if
       d2_next = d2_reg;
       d3_next = d3_reg; //added for minute digit 
       if (clr) //if clr is high 
-         begin
+         begin //reset all to zero if clr is pressed 
             d0_next = 4'b0;
             d1_next = 4'b0;
             d2_next = 4'b0;
-            d3_next = 4'b0; //reset all to zero if clr is pressed 
+            d3_next = 4'b0; 
+            //d0 = 1'b0; 
+            //d1 = 1'b0; 
+            //d2 = 1'b0; 
+            //d3 = 1'b0; 
          end
-      else if (ms_tick) //ms_tick == high 
-         if (d0_reg != 9) //if d0_reg != 9 
-            d0_next = d0_reg + 1; //increment next 
-         else              // reach XX9
-            begin
-               d0_next = 4'b0;
-               if (d1_reg != 9)
-                  d1_next = d1_reg + 1;
-               else       // reach X99
-                  begin
-                     d1_next = 4'b0;
-                     if (d2_reg != 5)
-                        d2_next = d2_reg + 1;
-                     else // reach 599
-                        begin 
-                            d2_next = 4'b0;//set seconds back to zero 
-                            if(d3_reg != 9) //reach 
-                                d3_next = d3_reg + 1; //increment minute digit 
-                            else 
-                                d3_next = 4'b0; //reset to 0.00.0
-                        end 
-                  end
-            end
-   end
+       //INCREMENT
+       else if (up) //if switch is selected to increment up==high
+        begin //1
+          if (ms_tick) //sample 
+             if (d0_reg != 9) //if d0_reg != 9 
+                d0_next = d0_reg + 1; //increment next 
+             else              // reach XX9
+                begin//2
+                   d0_next = 4'b0;
+                   if (d1_reg != 9)
+                      d1_next = d1_reg + 1;
+                   else       // reach X99
+                      begin//3
+                         d1_next = 4'b0;
+                         if (d2_reg != 5)
+                            d2_next = d2_reg + 1;
+                         else // reach 59.9
+                            begin //4
+                                d2_next = 4'b0;//set seconds back to zero - loop back around 
+                                if(d3_reg != 9) 
+                                    d3_next = d3_reg + 1; //increment minute digit 
+                                else 
+                                    d3_next = 4'b0; //reset to 0.00.0, i.e. reach 9.59.9
+                            end //4
+                      end//3
+                end//2
+             end//1
+                //DECREMENT
+           else //'up' switch is set to LOW, so when 'go' button is pressed stopwatch will decrement 
+            begin //1
+            if(ms_tick)
+              if (d0_reg != 0) //if d0_reg != 0
+                d0_next = d0_reg - 1; //decrement next 
+             else              // reach XX.0
+                begin //2 
+                   d0_next = 4'b1001; //set XX.9
+                   if (d1_reg != 0)
+                      d1_next = d1_reg - 1;
+                   else       // reach X0.0
+                      begin //3
+                         d1_next = 4'b1001;//set X9.9 
+                         if (d2_reg != 0)
+                            d2_next = d2_reg - 1;
+                         else // reach X.09.9
+                            begin //4
+                                d2_next = 4'b0101;//set 59.9 
+                                if(d3_reg != 0) //reach 
+                                    d3_next = d3_reg - 1; //increment minute digit 
+                                else 
+                                    d3_next = 4'b1001; //reset to 9.59.9 if we decrement below zero, wrap around 
+                            end //4
+                      end //3
+                end //2
+            end //1
+      end //end always statement 
    // output logic
    assign d0 = d0_reg;
    assign d1 = d1_reg;
--- StopwatchCode/stop_watch_test.v
+++ StopwatchCode/stop_watch_test.v
@@ -2,7 +2,7 @@
 module stop_watch_test
    (
     input wire clk,
-    input wire go, clr,
+    input wire go, clr,up,
     output wire [3:0] an,
     output wire [7:0] sseg
    );
@@ -12,16 +12,16 @@ module stop_watch_test
 
    // instantiate 7-seg LED display module
    disp_hex_mux disp_unit
-      (.clk(clk), .reset(1'b0), .hex3(d3),
+      (.clk(clk), .reset(1'b0), .hex3(d3), //add in the minute digit as an input 
         .hex2(d2), .hex1(d1), .hex0(d0),
        .dp_in(4'b0101), .an(an), .sseg(sseg)); //set M.SS.DS format 
 
    // instantiate stopwatch
    stop_watch_if counter_unit
-      (.clk(clk), .go(go), .clr(clr),
-       .d3(d3), .d2(d2), .d1(d1), .d0(d0) );
+      (.clk(clk), .go(go), .clr(clr), .up(up), //add in 'up' switch as an input 
+       .d3(d3), .d2(d2), .d1(d1), .d0(d0) ); //add minute digit as an output 
        
    //disable the unused display by setting it to 1
-   assign an = an | 4'b0000; //activate all digits 
+    assign an = an | 4'b0000; //activate all digits 
 
 endmodule
